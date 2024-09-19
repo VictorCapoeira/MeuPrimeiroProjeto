799943fb44f7c815fc78a591af72d364d9868642
Victor Emanuel <victorgit28@outlook.com>
2024-09-19 10:21:52 -0300

    Upando v2

--- PedraPapelTesoura/Program.cs
+++ PedraPapelTesoura/Program.cs
@@ -1,11 +1,11 @@
 ﻿Random rnd = new Random();
-int n = rnd.Next(1,3);
 Console.WriteLine("PEDRA PAPEL TESOURA!");
-string machEsc = n == 1 ? "pedra" : n == 2 ? "papel" : "tesoura";
 string userEsc;
 bool win = false;
 int winNum = 0;
     while(win == false){
+        int n = rnd.Next(1,3);
+        string machEsc = n == 1 ? "pedra" : n == 2 ? "papel" : "tesoura";
         Console.WriteLine("Escolha entre pedra, papel ou tesoura: ");
         userEsc = Console.ReadLine().ToLower();
         winNum++;
@@ -18,10 +18,14 @@ int winNum = 0;
         }else if(machEsc == "papel" && userEsc == "tesoura"){
             Console.WriteLine("Você venceu!!!");
             win = true;
-        }else{
-            Console.WriteLine("Você perdeu! Jogue novamente.");
         }
+        else if(machEsc == userEsc){
+            Console.WriteLine("Você empatou com a máquina! Jogue novamente.");
+        }else
+            Console.WriteLine("Você perdeu! Jogue novamente.");
     }
-    Console.WriteLine($"{winNum} tentativas para conseguir ganhar o jogo!");
-
-
+    string mensagem = winNum == 1 ? $"{winNum} tentativa para conseguir ganhar o jogo! Parabéns, você é muito bom!!!" : winNum > 1 && winNum <= 8 ? $"{winNum} tentativas para conseguir ganhar o jogo!" : $"{winNum} tentativas para conseguir ganhar o jogo! Você levou uma surra, pede benção para a máquina KKKKKKKKK";
+     
+    Console.WriteLine(mensagem);
+    
+    
